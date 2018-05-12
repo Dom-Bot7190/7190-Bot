ba5dc867884a7a12ea0c92580baff11bd186a31d
Dominic <dom.botelho@gmail.com>
2018-05-12 01:06:59 -0700

    Set up framework

--- src/org/usfirst/frc/team7190/robot/Commands/SwitchDrop.java
+++ src/org/usfirst/frc/team7190/robot/Commands/SwitchDrop.java
@@ -1,4 +1,21 @@
 package org.usfirst.frc.team7190.robot.Commands;
 
-public class SwitchDrop {
+import edu.wpi.first.wpilibj.command.Command;
+
+public class SwitchDrop extends Command {
+
+    public SwitchDrop() {
+        super("SwitchDrop");
+    }
+
+    protected void initialize() {
+    }
+
+    protected void execute() {
+    }
+
+    protected boolean isFinished() {
+        return false;
+    }
 }
+
